eac935c02171ad21437101ad78951d36cc5e9c3b
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2025-04-09 13:13:11 +0530

    Added midldleware logic

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+export const authenticateJWT = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
+  }
+
+  // Removing 'Bearer ' from the token if included
+  const tokenValue = token.startsWith("Bearer ") ? token.slice(7) : token;
+
+  try {
+    // Verifying the token
+    const decoded = jwt.verify(tokenValue, process.env.JWT_SECRET_KEY);
+    req.userId = decoded.userId;
+    req.role = decoded.role;
+    req.email = decoded.email;
+    req.institutionDomain = decoded.institutionDomain;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid token." });
+  }
+};
