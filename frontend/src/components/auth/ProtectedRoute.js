4a6150bd0e3397a8cb7fc5fe1a1440f524d5a183
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2025-04-10 13:13:09 +0530

    Added auth part

---
+++ frontend/src/components/auth/ProtectedRoute.js
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+
+const ProtectedRoute = ({ children, allowedRoles }) => {
+  const user = useSelector((state) => state.auth.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/admin-login", { replace: true }); // Redirect if not logged in
+    } else if (!allowedRoles.includes(user.role)) {
+      navigate("/", { replace: true }); // Redirect if unauthorized
+    }
+  }, [user, allowedRoles, navigate]);
+
+  if (!user || !allowedRoles.includes(user?.role)) {
+    return null; // Prevent rendering while navigating
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
