329646e018126899010687ed040457bb75738e54
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2025-04-11 13:28:59 +0530

    Added changes to home part

--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -1,4 +1,4 @@
-"use client";
+;
 
 import { motion } from "framer-motion";
 import { TypeAnimation } from "react-type-animation";
--- frontend/src/components/home/PieChartComponent.jsx
+++ frontend/src/components/home/PieChartComponent.jsx
@@ -1,4 +1,4 @@
-"use client";
+;
 
 import { useMemo } from "react";
 import { Label, Pie, PieChart } from "recharts";
