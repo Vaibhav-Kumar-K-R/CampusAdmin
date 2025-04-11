03c5bb1e9bd0a9c839f25654fd2d1f7e8dc327ea
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2025-04-11 17:33:53 +0530

    Cors update

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,10 @@ connectDB();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: "https://erpcampusadmin.netlify.app/",
+  credentials: true
+}));
 
 app.get("/", (req, res) => {
   res.send("Hello, server is running!");
