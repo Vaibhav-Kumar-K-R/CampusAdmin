166a6904372cd3f46f54d74f09e003cf30620209
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2025-04-11 13:30:27 +0530

    edux added

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,32 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Uses localStorage
+import authSlice from "./authSlice"; // Adjust the path if needed
+
+// Persist Config
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Combine reducers
+const rootReducer = combineReducers({
+  auth: authSlice,
+});
+
+// Create persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Required for Redux Persist
+    }),
+});
+
+// Create persistor
+export const persistor = persistStore(store);
+
+export default store;
